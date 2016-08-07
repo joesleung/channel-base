3edc392af706cee91f96555c5719ace7ca1a9169
youing <paulwang@vip.qq.com>
2016-08-08 00:24:52 +0800

    add commit

--- src/base/main.js
+++ src/base/main.js
@@ -1,11 +1,14 @@
 define(function (require) {
   'use strict';
+  //给html标签打上频道类名，主要用作重置头部样式
   var o2AppName = pageConfig.o2AppName || '';
   if(o2AppName.length === 0) {
     $('html').addClass(o2AppName);
   }
+  //console 输出
   var o2console = require('o2console');
   o2console.consoleConfigFunc();
+  //加载主站头部公共脚本
 	require.async(['jdf/1.0.0/unit/globalInit/2.0.0/globalInit.js', 'jdf/1.0.0/unit/category/2.0.0/category.js'], function(globalInit, category) {
     globalInit();
     category({
@@ -13,7 +16,11 @@ define(function (require) {
       mainId: '#categorys-mini',
       el: '#categorys-mini-main'
     });
+
+    //图片懒加载
     require('o2lazyload');
+
+    //绑定渲染事件
     $('body').o2lazyload().bind('render', '.o2data-lazyload', function(e, result) {
       var self = $(e.target);
       var template = self.find('[type="text/template"]');
@@ -24,12 +31,15 @@ define(function (require) {
       } else {
         content = template.html();
       }
+
+      //加载模板引擎
       var o2tpl = require('o2tpl');
       try {
         var html = o2tpl(content, data[self.data('id')]);
         template.remove();
         self.append($(html));
         setTimeout(function(){
+          //触发脚本
           self.trigger('done');
           '' !== script && (new Function(script))();
           $(window).trigger('resize');
@@ -39,6 +49,7 @@ define(function (require) {
         console.log(e);
       }
     });
+    //楼层懒加载逻辑
     var o2widgetLazyload = require('o2widgetLazyload');
     o2widgetLazyload();
   });
--- src/base/widget_lazyload.js
+++ src/base/widget_lazyload.js
@@ -5,15 +5,24 @@ define('o2widgetLazyload', function(require, exports, module) {
 			cls: 'o2data-lazyload',
 			scrollEvent: 'scroll.lazydata resize.lazydata'
 		};
+		/**
+		 * @desc o2JSConfig 异步模板配置
+		 *
+		 */
 		var o2JSConfig = window.pageConfig ? window.pageConfig.o2JSConfig : {};
 		o2JSConfig = o2JSConfig || {};
 		$.extend(conf, options);
-    var store = require('store');
+		//本地存储库
+    	var store = require('store');
 		var init = function() {
 			var scrollTimer = null;
 			$(window).bind(conf.scrollEvent, function(e) {
 				clearTimeout(scrollTimer);
 				scrollTimer = setTimeout(function () {
+					/**
+					 * @var preloadOffset 可视区域阀值，用作提前渲染楼层
+					 *
+					 */
 					var preloadOffset = /msie/i.test(navigator.userAgent) ? 1000 : 500;
 					var st = $(document).scrollTop(),
 						wh = $(window).height() + preloadOffset,
@@ -29,9 +38,17 @@ define('o2widgetLazyload', function(require, exports, module) {
 							dataAsync = self.data('async'),
 							forceRender = typeof self.data('forcerender') === 'boolean' ? self.data('forcerender') : false,
 							tplPath = null;
-							
-							if (forceRender || ($(rel).offset().top - (st + wh) < 0 && $(rel).offset().top + $(rel).outerHeight(true) >= st)) {
+							/**
+							 *@desc 可视区域渲染模板，根据tplVersion从localstorage读取模板，IE浏览器直接异步加载。
+							 *@var data-tpl{string} 模板ID
+							 *@var data-async{boolean} 是否同步渲染，即渲染模板前进行 beforerender 事件处理，回调后再渲染模板
+							 *@var data-forcerender{boolean} 强制渲染，用作某些需要直接渲染的楼层
+							 *@var data-rel{object} 参考渲染对象，默认是本身
+							 */
 
+							//判断是否是在可视区域 || 是否强制渲染
+							if (forceRender || ($(rel).offset().top - (st + wh) < 0 && $(rel).offset().top + $(rel).outerHeight(true) >= st)) {
+							
 							if (tplId && o2JSConfig.pathRule) {
 								tplPath = o2JSConfig.pathRule(tplId);
 								var isIE = !!window.ActiveXObject || navigator.userAgent.indexOf("Trident") > 0;
