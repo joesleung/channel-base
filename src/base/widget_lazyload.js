5c38cfecc222461bd5412717053c6497cc3a8f08
youing <paulwang@vip.qq.com>
2016-08-08 00:36:11 +0800

    注释

--- src/base/widget_lazyload.js
+++ src/base/widget_lazyload.js
@@ -20,7 +20,7 @@ define('o2widgetLazyload', function(require, exports, module) {
 				clearTimeout(scrollTimer);
 				scrollTimer = setTimeout(function () {
 					/**
-					 * @var preloadOffset 可视区域阀值，用作提前渲染楼层
+					 * @desc preloadOffset 可视区域阀值，用作提前渲染楼层
 					 *
 					 */
 					var preloadOffset = /msie/i.test(navigator.userAgent) ? 1000 : 500;
@@ -39,11 +39,11 @@ define('o2widgetLazyload', function(require, exports, module) {
 							forceRender = typeof self.data('forcerender') === 'boolean' ? self.data('forcerender') : false,
 							tplPath = null;
 							/**
-							 *@desc 可视区域渲染模板，根据tplVersion从localstorage读取模板，IE浏览器直接异步加载。
-							 *@var data-tpl{string} 模板ID
-							 *@var data-async{boolean} 是否同步渲染，即渲染模板前进行 beforerender 事件处理，回调后再渲染模板
-							 *@var data-forcerender{boolean} 强制渲染，用作某些需要直接渲染的楼层
-							 *@var data-rel{object} 参考渲染对象，默认是本身
+							 * @desc 可视区域渲染模板，根据tplVersion从localstorage读取模板，IE浏览器直接异步加载。
+							 * data-tpl{string} 模板ID
+							 * data-async{boolean} 是否同步渲染，即渲染模板前进行 beforerender 事件处理，回调后再渲染模板
+							 * data-forcerender{boolean} 强制渲染，用作某些需要直接渲染的楼层
+							 * data-rel{object} 参考渲染对象，默认是本身
 							 */
 
 							//判断是否是在可视区域 || 是否强制渲染
