9b4608a99188e05aedfa5bc4bee5645922128762
o2team <o2team-ci@jd.com>
2016-12-22 07:07:59 +0000

    Deployed to Github Pages [skip ci]

---
+++ build/components/login/1.0.0/login.js
@@ -0,0 +1,1 @@
+define("//static.360buyimg.com/mtd/pc/components/login/1.0.0/login.js",["//misc.360buyimg.com/jdf/1.0.0/unit/login/1.0.0/login.js"],function(i){"use strict";var n=i("//misc.360buyimg.com/jdf/1.0.0/unit/login/1.0.0/login.js"),o=_.Class.extend({construct:function(i){$.extend(this,{},i)},isLogin:function(i){"function"==typeof i?n.isLogin(i):n(i)}}),t=(new o).isLogin;return t});
